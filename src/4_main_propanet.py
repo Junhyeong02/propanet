5106915d0a9d0f79a95a0294ba8d6f5995d9c99b
Junhyeong02 <denovokjh@snu.ac.kr>
2023-02-24 17:40:26 +0900

    4_main_propanet.py

--- src/4_main_propanet.py
+++ src/4_main_propanet.py
@@ -2,14 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 import argparse
-import sys
 import networkx as nx
 import time
+from tqdm import tqdm
 import utils.propagation as NP
 import utils.target_gene as TG 
-import multiprocessing
-from glob import glob
-from tqdm import tqdm
 
 def infByNode(TF, g, TFset) :
     visited = set()
@@ -39,11 +36,11 @@ def IM(nwk,TFset,repeat) :
         infNo (dict): reachability for each TF
         TFRank (list): sorted TF list
     '''
-    nodes=pd.Series(list(set([x for x in nwk.nodes()])))
+
     infNo = {}
     for n in TFset :
         infNo[n]=0.0
-    for i in range(repeat) :
+    for _ in tqdm(range(repeat)):
         # Produce G'
         g = nwk.copy()
         #for (a,b) in network.edges() :
@@ -51,7 +48,7 @@ def IM(nwk,TFset,repeat) :
         #        g.remove_edge(a, b)
                 #Calculate influcence (profit)
         for TF in TFset :
-            profit, lenInf, lenInfTG = infByNode(TF, g, TFset)
+            profit, lenInf, _ = infByNode(TF, g, TFset)
             #print TF,profit,lenInf,lenInfTG
             if lenInf>0 and not np.isnan(profit/float(lenInf)) : infNo[TF] += profit/float(lenInf)
     for n in TFset:
@@ -84,7 +81,6 @@ def TF_adding_NP(DEGli, geneSet, TFli, TFrankFile, DEGnetFile, seed, coverNo=200
     TFset=set(TFli)
     nodeCnt=len(set(DEGnet.nodes())-TFset)
     TFli_rank=pd.read_csv(TFrankFile,sep='\t',header=None)[0].tolist()
-    corPre=-np.inf
     corr=[]
     cover=[]
     TF_trimmed=[]
@@ -92,7 +88,7 @@ def TF_adding_NP(DEGli, geneSet, TFli, TFrankFile, DEGnetFile, seed, coverNo=200
     prevCor = 0
     currTol = 0
     spearmanLi = []
-    for iTF,TF in enumerate(TFli_rank):
+    for iTF,TF in tqdm(enumerate(TFli_rank)):
         #seedFile: only selected TF( TFli[:iTF] ) is marked, otherwise 0
         TF_trimmed.append(TFli_rank[iTF])
         seed2weight = seed.loc[TF_trimmed,:].T.to_dict('records')[0]
@@ -108,159 +104,97 @@ def TF_adding_NP(DEGli, geneSet, TFli, TFrankFile, DEGnetFile, seed, coverNo=200
             TF_trimmed.pop()
             continue
         TG_set |= TGtmp
-        # print '-----TF\t',TF_trimmed[-1]
-        # print 'correlation\t%.3f' % corTmp
-        # print 'len TG_set\t', len(TG_set)
+
         c=float(len(TG_set))/nodeCnt
-        # print 'coverage\t%.3f' % c
         cover.append(c)
+
         if prevCor > corTmp : currTol +=1
         else : currTol = 0
         if coverage != None and (cover[-1] > coverage or len(TG_set)>coverNo):
             TF_trimmed.pop()
             break
-        corPre = corTmp
     seed2weight = seed.loc[TF_trimmed,:].T.to_dict('records')[0]
     _, lst_node_weight = wk.run_exp(seed2weight, TFset, 0.1, normalize=False)
     return spearmanLi, cover, TF_trimmed, lst_node_weight
     
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser()
-    parser.add_argument('-TFliFile',help='TF list File')
-    parser.add_argument('-nwkFile',help='Network file')
-    parser.add_argument('-expFile',help='gene expression file with z-value,seed file for NP')
-    parser.add_argument('-binFile',help='binary file of condition from DEGlimma')
-    parser.add_argument('-geneSet',help='gene set by user') 
-    parser.add_argument('-cond',required=True,help='condition name') 
-    parser.add_argument('-outD',help='project (output directory) name')
+    parser.add_argument('--tf',help='TF list File')
+    parser.add_argument('--nwk',help='Network file')
+    parser.add_argument('--lvl',help='DE level of genes')
+    parser.add_argument('--deg',help='deg list file')
+    parser.add_argument('--out',help='project (output directory) name')
+    parser.add_argument('--imround', help="influence maximization round", default=1000)
     parser.add_argument('-p',default='10',type=int,help='# process for multiprocessing')
     parser.add_argument('-c',default='0.5',type=float,help='coverage threshold', choices=range(0, 1))
     parser.add_argument('-coverNo',default='200',type=float)
-    parser.add_argument('-N', required=True, help= "number of samples", type = int)
     args=parser.parse_args()
 
-    if args.outD != None:
-        if not os.path.exists(args.outD) : os.mkdir(args.outD)
-    exp = pd.read_csv(args.expFile, sep='\t',index_col=0)
-    with open(args.TFliFile) as tfF:
-        TFli=tfF.read().strip().split()
+    if not os.path.exists(os.path.dirname(args.out)): 
+        os.makedirs(os.path.dirname(args.out))
+
+    with open(args.tf) as f:
+        tfli = f.read().strip().split()
+
+    with open(args.deg) as f:
+        degli = set(f.read().strip().split())
    
     #networkFile --> networkx object, expFile --> pd.DataFrame
-    print('TF length', len(set(TFli)))
+    print('Number of transcription factors', len(set(tfli)))
+    print('Number of differenitial expressed genes', len(set(degli)))
     
-    n = args.N
-    assert n == len(exp.columns)
-    
-    networks = []
-    for nwkFile in tqdm(glob(args.nwkFile + "*.tsv")):
-        network = nx.read_edgelist(nwkFile,data=(('weight',float),),create_using=nx.DiGraph()) 
-        networks.append(network)
+    network = nx.read_edgelist(args.nwk, data=(('weight',float),),create_using=nx.DiGraph())
 
-    print(networks)
-
-    bins = pd.read_csv(args.binFile, sep='\t',index_col=0)
-    weight = (exp*(bins.applymap(lambda x:abs(x))))
     #IM, NP for each timepoint
+    print(network)
 
-    def main_fxn(i):
-
-        network = networks[i-1]
-        print(network)
-
-        #step0: parsing exp data, network for each timepoint
-        start=time.time() 
-        weight_tp = exp.iloc[:,i-1].reset_index()
-        weight_tp.columns = ['gene','weight']
-
-        DEG_tp =  weight.iloc[:,i-1].reset_index()
-        DEG_tp.columns = ['gene','weight']
-        DEGli = DEG_tp[DEG_tp['weight']!=0]['gene'].tolist()
-         
-        DEGliFile =args.outD+'/'+ args.cond+'.DEGli.t'+str(i)
-        
-        with open(DEGliFile,'w') as f:
-            f.write('\n'.join(DEGli))
-        if args.geneSet != None:
-            geneSet = set(np.genfromtxt(args.geneSet, dtype=np.str))&DEGli
-        else:
-            geneSet = set(DEGli)
-
-        ##Make nwk with DEGs in specific timepoint
-
-        nwk_tp = network.subgraph(geneSet|set(TFli))
-        DEGnetFile = args.outD + '/' + args.cond + '.nwk.t'+ str(i) 
-        
-        nx.write_edgelist(nwk_tp,DEGnetFile,data=['weight'],delimiter='\t')
-        ##Assign new weight to timepoint-specific network
-        weight_DEG_nwk = weight_tp[weight_tp.gene.isin(nwk_tp.nodes())].set_index('gene')
-        weight_dict = weight_DEG_nwk.T.to_dict('records')[0]
-        nx.set_node_attributes(nwk_tp,weight_dict,'weight')
-        
-        #step1: Influence maximization
-        start=time.time() 
-        ##preprocess
-        nodes=pd.Series(list(set([x for x in nwk_tp.nodes()])))
-        TFset=set(nodes[nodes.isin(TFli)].tolist())
-        ##influence maximization
-        TFrank, infNo = IM(nwk_tp,TFset,1)
-        ##Output into file 
-        TFrankFile =args.outD+'/'+ args.cond+'.TF_rank.t'+str(i)  
-
-
-        with open(TFrankFile,'w') as TFrankF:
-            for TF in TFrank:
-                TFrankF.write(TF+'\n')#IM result--> cold.D2.TF_rank.1
-
-        print(sys.stderr,'time point', i,'TF ranking done', time.strftime("%H:%M:%S",time.gmtime(time.time()-start)), '\n')
+    #step0: parsing exp data, network for each timepoint
+    start=time.time() 
     
-        
-        #step2: NP based on TF rank
-        start=time.time() 
-        ##TF adding NP
-        seed = weight_tp.set_index('gene')
-        spearmanLi,coverage,TF_trimmed,lst_node_weight = TF_adding_NP(DEGli, geneSet, TFli, TFrankFile, DEGnetFile, seed, args.coverNo, coverage=args.c) 
-        
-        ##Save Output as file 
-        TFtrimF = TFrankFile + '.trim'
+    weight_tp = pd.read_csv(args.lvl, sep = "\t")
+    DEG_tp = weight_tp.where(weight_tp["gene"].isin(set(degli)), other = 0)
 
-        with open(TFtrimF,'w') as f:
-            f.write('\n'.join(TF_trimmed))
-        
-        dic_node2weights = {}
-        column_name = exp.columns.tolist()
-        lst_seed = exp.index.values.tolist()
-        wk = NP.Walker(DEGnetFile, absWeight=True)
-        set_nodes = set(wk.OG.nodes())
-        set_tmpNodes = set()
-        for node, weight_np, all_weight in lst_node_weight:
-            if node not in dic_node2weights:
-                dic_node2weights[node]=[]
-            dic_node2weights[node].append(weight_np)
-            set_tmpNodes.add(node)
-        
-        for node in set_nodes-set_tmpNodes:
-            if node not in dic_node2weights:
-                dic_node2weights[node]=[]
-            dic_node2weights[node].append(0.0)
+    DEGliFile = args.deg
 
-        print(DEGliFile, TFrankFile, TFtrimF)
-
-        return 0
+    ##Make nwk with DEGs in specific timepoint
+    network = network.subgraph(set(degli)|set(tfli))
+    DEGnetFile = args.out + ".nwk" 
+    nx.write_edgelist(network,DEGnetFile,data=['weight'],delimiter='\t')
     
-    """
-    pool = multiprocessing.Pool(args.p)
-    nums=range(1,n+1)
-    res = pool.imap_unordered(main_fxn,nums)
+    ##Assign new weight to timepoint-specific network
+    weight_DEG_nwk = weight_tp[weight_tp["gene"].isin(network.nodes())].set_index('gene')
+    weight_dict = weight_DEG_nwk.T.to_dict('records')[0]
+    nx.set_node_attributes(network,weight_dict,'weight')
     
-    time.sleep(10)
+    #step1: Influence maximization
+    start=time.time() 
+    ##preprocess
+    nodes=pd.Series(list(set([x for x in network.nodes()])))
+    TFset=set(nodes[nodes.isin(tfli)].tolist())
+    ##influence maximization
+    TFrank, infNo = IM(network,TFset, int(args.imround))
+    ##Output into file 
+    TFrankFile = args.out + '.tf.rank'
 
-    pool.close()
-    pool.join()    
-    """
+    with open(TFrankFile,'w') as TFrankF:
+        for TF in TFrank:
+            TFrankF.write(TF+'\n')#IM result--> cold.D2.TF_rank.1
 
-    for i in tqdm(range(1, n+1)):
-        main_fxn(i)
+    print('TF ranking done', time.strftime("%H:%M:%S",time.gmtime(time.time()-start)), '\n')
+    
+    #step2: NP based on TF rank
+    start=time.time() 
+    ##TF adding NP
+    seed = weight_tp.set_index('gene')
+    spearmanLi,coverage,TF_trimmed,lst_node_weight = TF_adding_NP(degli, degli, tfli, TFrankFile, DEGnetFile, seed, args.coverNo, coverage=args.c) 
+    
+    ##Save Output as file 
+    TFtrimF = TFrankFile + '.trim'
 
+    with open(TFtrimF,'w') as f:
+        f.write('\n'.join(TF_trimmed))
+
+    print(DEGliFile, TFrankFile, TFtrimF)
     print("main process end")
 
 
